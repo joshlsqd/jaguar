23dbf450392887042fc6da6efcede4865b9f76e1
Joshua Cook <jcook@lynxsolutions.com>
2018-04-09 16:26:32 -0700

    Taskform and auth

--- jaguar-client/src/components/TaskForm.js
+++ jaguar-client/src/components/TaskForm.js
@@ -1,37 +1,60 @@
 import React from 'react';
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
+import {RaisedButton} from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-const  CreateTask = gql`
-    mutation CreateTask($tasktitle: String!){
-        createTask(tasktitle: $tasktitle){
-            _id
-            tasktitle
-        }
-    }
+
+const ADD_TODO = gql`
+ mutation addTodo($type: String!) {
+   addTodo(type: $type) {
+     id
+     type
+   }
+ }
 `;
 
-const TaskForm = () => {
-    let ;
+const GET_TODOS  =  gql`
+   query ReadTodo {
+     todo(id: 5) {
+       id
+       text
+       completed
+     }
+   }
+ `;
 
+
+const TaskForm = () => {
+    let input;
 
     return (
-        <Mutation mutation={CreateTask}>
-            {(createTask, { data }) => (
+        <Mutation
+            mutation={ADD_TODO}
+            update={(cache, { data: { addTodo } }) => {
+                const { todos } = cache.readQuery({ query: GET_TODOS });
+                cache.writeQuery({
+                    query: GET_TODOS,
+                    data: { todos: todos.concat([addTodo]) }
+                });
+            }}
+        >
+            {addTodo => (
                 <div>
                     <form
                         onSubmit={e => {
                             e.preventDefault();
-                            createTask({ variables: { tasktitle: input.value } });
+                            addTodo({ variables: { type: input.value } });
+                            console.log(input.value)
                             input.value = "";
                         }}
                     >
                         <input
                             ref={node => {
-                                inputuser = node;
+                                input = node;
                             }}
                         />
-                        <button type="submit">Add Task</button>
+                        <button type="submit">Add Todo</button>
                     </form>
                 </div>
             )}
--- jaguar-client/src/containers/App.js
+++ jaguar-client/src/containers/App.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 import { ApolloProvider} from "react-apollo";
 import ApolloClient from 'apollo-boost';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from '../containers/Navbar';
 import SignupForm from '../components/SignupForm';
+import TaskForm from "../components/TaskForm";
+
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
 
 const client = new ApolloClient({
     uri: "http://localhost:3001/graphql"
@@ -12,14 +28,17 @@ const client = new ApolloClient({
 class App extends Component {
     render(){
         return(
+            <MuiThemeProvider>
             <ApolloProvider client={client}>
                 <Router>
+                    <Route path="taskform" component={TaskForm} />
                 <div>
                 <Navbar/>
                 <SignupForm/>
                 </div>
                 </Router>
             </ApolloProvider>
+            </MuiThemeProvider>
         )
     }
 }
